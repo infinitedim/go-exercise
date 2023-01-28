035281ec85b5584dc0c89d0ffb4a3ae4f01845c4
infinitedim <saputradimas.boediluhur@gmail.com>
2023-01-28 09:09:47 +0700

    init

---
+++ datatypes/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dimas")
+	// Function untuk menghitung jumlah string (spasi termasuk)
+	fmt.Println(len("Dimas Saputra"))
+	// Function untuk mengambil karakter ke n ke dalam byte
+	fmt.Println("Ini adalah string"[1])
+}
---
+++ datatypes/variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Dimas Saputra"
+	fmt.Println(name)
+
+	name = "Baby blooo"
+	fmt.Println(name)
+}
